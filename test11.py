f023a6dc976219aee1b9792dc0d75041f04eb63d
Imanghasemizadeh <140438269+Imanghasemizadeh@users.noreply.github.com>
2023-08-04 16:31:43 +0330

    project2_dice.py

--- test11.py
+++ test11.py
@@ -3,21 +3,22 @@ print("in the name of Allah")
 print("if you wana play choose : S , if you dont wana play choose : E")
 
 import random
-
+roll = random.randint(1,6)
+gift = random.randint(1,6)
 while True :
     player_choose = input("your choice : ")
-
-    if player_choose == "S" :  
-       roll = random.randint(1,6)
-       print("your number is : ", roll)
-       if roll == 6 :
-        print("nice one you can roll again")
-        roll_num_2 = random.randint(1,6)
-        print("your second_roll number is :" , roll_num_2)
-       else :
-          break
+    if player_choose == "S" : 
+        print("your number is : ", roll)
+        if roll == 6 :
+            print("nice one you can roll again : " , gift)
+            while gift >= 6 :
+                print("your  number is :" , gift)
+            if gift <6 :
+                break
+            else :
+                  break
     if player_choose == "E" :
        break
     else :
-       print("wrong choice")
+        break        
 print("good luck and have fun")
